aef95fb1e44d84b67b61eddca563e940a5ef2062
Jadala Ramani <jadalaramani@gmail.com>
2025-08-12 19:58:30 +0530

    Update variables.tf

--- us-west-2/modules/network/variables.tf
+++ us-west-2/modules/network/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
 variable "dr_region" {
   type = string
@@ -22,8 +22,8 @@ variable "public_subnets" {
   }))
 
   # default = [
-  #   { cidr = "170.20.1.0/24", az = "us-east-1a" },
-  #   { cidr = "170.20.2.0/24", az = "us-east-1b" }
+  #   { cidr = "170.20.1.0/24", az = "us-west-2a" },
+  #   { cidr = "170.20.2.0/24", az = "us-west-2b" }
   # ]
 }
 
@@ -34,12 +34,12 @@ variable "private_subnets" {
     az   = string
   }))
   # default = [
-  #   { cidr = "170.20.3.0/24", az = "us-east-1a" },
-  #   { cidr = "170.20.4.0/24", az = "us-east-1b" },
-  #   { cidr = "170.20.5.0/24", az = "us-east-1a" },
-  #   { cidr = "170.20.6.0/24", az = "us-east-1b" },
-  #   { cidr = "170.20.7.0/24", az = "us-east-1a" },
-  #   { cidr = "170.20.8.0/24", az = "us-east-1b" }
+  #   { cidr = "170.20.3.0/24", az = "us-west-2a" },
+  #   { cidr = "170.20.4.0/24", az = "us-west-2b" },
+  #   { cidr = "170.20.5.0/24", az = "us-west-2a" },
+  #   { cidr = "170.20.6.0/24", az = "us-west-2b" },
+  #   { cidr = "170.20.7.0/24", az = "us-west-2a" },
+  #   { cidr = "170.20.8.0/24", az = "us-west-2b" }
   # ]
 }
 # variable "private_subnet_cidrs" {
