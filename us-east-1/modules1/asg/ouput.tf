5726e12e0a094d22c450b1656c82f513bb8e3e22
Jadala Ramani <jadalaramani@gmail.com>
2025-08-12 21:36:42 +0530

    Delete us-east-1/modules1 directory

--- us-east-1/modules1/asg/ouput.tf
+++
@@ -1,9 +0,0 @@
-output "frontend_asg_id" {
-  description = "Frontend Auto Scaling Group ID"
-  value       = aws_autoscaling_group.frontend_asg.id
-}
-
-output "backend_asg_id" {
-  description = "Backend Auto Scaling Group ID"
-  value       = aws_autoscaling_group.backend_asg.id
-}
