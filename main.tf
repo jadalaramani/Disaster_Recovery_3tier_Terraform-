bd7b5f581ce78c209b4d8cbfb79fbd1a01aafc4d
Jadala Ramani <jadalaramani@gmail.com>
2025-08-12 20:16:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -77,20 +77,20 @@ value = module.security_group.security_group_id
 module "secondary_network" {
   source = "./us-west-2/modules/network"
 
-  vpc_cidr =  "170.20.0.0/16"
+  vpc_cidr =  "170.21.0.0/16"
   
    public_subnets = [  
-    { cidr = "170.20.1.0/24", az = "us-west-2a" },
-    { cidr = "170.20.2.0/24", az = "us-west-2b" }
+    { cidr = "170.21.1.0/24", az = "us-west-2a" },
+    { cidr = "170.21.2.0/24", az = "us-west-2b" }
    ]
 
    private_subnets =  [
-    { cidr = "170.20.3.0/24", az = "us-west-2a" },
-    { cidr = "170.20.4.0/24", az = "us-west-2b" },
-    { cidr = "170.20.5.0/24", az = "us-west-2a" },
-    { cidr = "170.20.6.0/24", az = "us-west-2b" },
-    { cidr = "170.20.7.0/24", az = "us-west-2a" },
-    { cidr = "170.20.8.0/24", az = "us-west-2b" }
+    { cidr = "170.21.3.0/24", az = "us-west-2a" },
+    { cidr = "170.21.4.0/24", az = "us-west-2b" },
+    { cidr = "170.21.5.0/24", az = "us-west-2a" },
+    { cidr = "170.21.6.0/24", az = "us-west-2b" },
+    { cidr = "170.21.7.0/24", az = "us-west-2a" },
+    { cidr = "170.21.8.0/24", az = "us-west-2b" }
   ]
 }
 
