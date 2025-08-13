8f0267b8fb3eeb35305710d65db45d25181d7249
Jadala Ramani <jadalaramani@gmail.com>
2025-08-13 11:56:55 +0530

    Update output.tf

--- us-east-1/modules/rds/output.tf
+++ us-east-1/modules/rds/output.tf
@@ -12,3 +12,7 @@ output "rds_subnet_group" {
   description = "The name of the RDS subnet group"
   value       = aws_db_subnet_group.rds_subnet_group.name
 }
+
+output "db_arn" {
+  value = var.replicate_source_db == "" ? aws_db_instance.primary[0].arn : aws_db_instance.replica[0].arn
+}
