977c23e02ee9cf15bf02e0553545a151af24ee20
Jadala Ramani <jadalaramani@gmail.com>
2025-08-13 12:46:33 +0530

    Update main.tf

--- us-east-1/modules/primary/main.tf
+++ us-east-1/modules/primary/main.tf
@@ -1,13 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-provider "aws" {
-  alias  = "secondary"
-  region = "us-west-2"
-}
-
-
 # DB Subnet Group
 resource "aws_db_subnet_group" "db_subnet_group" {
   name       = "${var.db_identifier}-subnet-group"
